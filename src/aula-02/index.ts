0d67bcace35b2d63545eea337abb077fadbaeda8
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-20 19:02:15 -0300

    Lesson 02: Primitive types

---
+++ src/aula-02/index.ts
@@ -0,0 +1,18 @@
+/*
+    Tipos primitivos
+*/
+
+// String
+let userName: string;
+
+userName = 'Pedro';
+
+// Number
+let total: number;
+total = 3;
+total = 7.5;
+
+// Boolean
+let isLoading: boolean;
+isLoading = true;
+isLoading = false;
