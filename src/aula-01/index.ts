ebaa42a85d8e6169e2ee32e699f84abeff7a8132
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-20 18:51:06 -0300

    Lesson 01: Static type checking

--- src/aula-01/index.ts
+++ src/aula-01/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello TypeScript!');
+/*
+    Verificação de tipo estático
+*/
+
+const message = 'Hello TypeScript';
+
+// Consigo mudar?
+// message = 'TypeScript';
+
+// Consigo execultar?
+// message();
+
+const user = {
+  name: 'Pedro',
+  email: 'email.com',
+};
+
+// Propriedade existe?
+// console.log(user.avatar)
