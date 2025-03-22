29d4404be9bd99c09d54057a8c770c067329fa55
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-22 16:59:52 -0300

    Lesson 05: Null and Undefined

---
+++ src/aula-05/index.ts
@@ -0,0 +1,16 @@
+/*
+    Null e Undefined
+*/
+
+// Undefined
+let value: number;
+value = 12; // Sem essa linha retorna Undefined
+console.log(value);
+
+// Null
+let email = null;
+console.log(email);
+
+if (!email) {
+  console.log('Informe o email!');
+}
