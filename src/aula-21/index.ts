6f0bfbcbd512d4bc27ff68748af2d338cc4a4dc2
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-03-27 22:16:05 -0300

    Lesson 21: Keyof

---
+++ src/aula-21/index.ts
@@ -0,0 +1,13 @@
+/*
+    Keyof
+*/
+
+const icons = {
+  home: './path/home.svg',
+  add: './path/add.svg',
+  remove: './path/remove.svg',
+};
+
+type Icon = typeof icons;
+
+const icon: keyof Icon = 'add';
